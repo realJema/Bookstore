3c73a39d3623d0db44f9b36b3482536e68681f07
realjema <realjema@gmail.com>
2022-01-12 15:22:20 +0100

    best seelers start

--- src/views/Index.js
+++ src/views/Index.js
@@ -38,25 +38,28 @@ class Billboard extends React.Component {
         <MainNavbar />
         <Adslider />
         <main ref="main">
-          <section className="section section-components pb-0">
+          <section className="section section-components pb-0 pt-0">
             <Container className="col-md-11 col-lg-11">
-              <Row className="row-grid justify-content-between">
-                <Col sm="9">
-                  <h2 className="mt-md mb-5 display-4">
-                    <span>Featured Categories</span>
-                  </h2>
-                </Col>
-                <Col sm="3" className="text-md-right">
-                  <h6 className="mt-lg mb-5">
-                    <span>All Categories</span>
-                  </h6>
-                </Col>
+              <Row className="row-grid justify-content-between align-items-end">
+                <h2 className="mt-md mb-5 display-4">
+                  <span>Featured Categories</span>
+                </h2>
+                <h6 className="mt-lg mb-5">
+                  <span>All Categories</span>{" "}
+                  <i className="fa fa-chevron-right"></i>
+                </h6>
               </Row>
               <Row className="justify-content-center">
                 <Col lg="12">
                   <Row className="row-grid">
                     <Col lg="2" className="featured-category">
-                      <Card className="col mb-5" style={{backgroundColor: "#faf1ff", border: "#faf1ff"}}>
+                      <Card
+                        className="col mb-5"
+                        style={{
+                          backgroundColor: "#faf1ff",
+                          border: "#faf1ff",
+                        }}
+                      >
                         <CardBody>
                           <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
                             <i className="ni ni-satisfied" />
@@ -64,14 +67,18 @@ class Billboard extends React.Component {
                           <h6 className="text-primary text-uppercase">
                             Arts & Photography
                           </h6>
-                          <p className="text-primary ">
-                            Shop Now
-                          </p>
+                          <p className="text-primary ">Shop Now</p>
                         </CardBody>
                       </Card>
                     </Col>
                     <Col lg="2" className="featured-category">
-                      <Card className="col mb-5" style={{backgroundColor: "#faf4eb", border: "#faf4eb"}}>
+                      <Card
+                        className="col mb-5"
+                        style={{
+                          backgroundColor: "#faf4eb",
+                          border: "#faf4eb",
+                        }}
+                      >
                         <CardBody>
                           <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
                             <i className="ni ni-shop" />
@@ -79,14 +86,18 @@ class Billboard extends React.Component {
                           <h6 className="text-primary text-uppercase">
                             Food & Drink
                           </h6>
-                          <p className="text-primary">
-                            Shop Now
-                          </p>
+                          <p className="text-primary">Shop Now</p>
                         </CardBody>
                       </Card>
                     </Col>
                     <Col lg="2" className="featured-category">
-                      <Card className="col mb-5" style={{backgroundColor: "#f4e6e5", border: "#f4e6e5"}}>
+                      <Card
+                        className="col mb-5"
+                        style={{
+                          backgroundColor: "#f4e6e5",
+                          border: "#f4e6e5",
+                        }}
+                      >
                         <CardBody>
                           <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
                             <i className="ni ni-diamond" />
@@ -101,13 +112,19 @@ class Billboard extends React.Component {
                       </Card>
                     </Col>
                     <Col lg="2" className="featured-category">
-                      <Card className="col mb-5" style={{backgroundColor: "#e6f2f4", border: "#e6f2f4"}}>
+                      <Card
+                        className="col mb-5"
+                        style={{
+                          backgroundColor: "#e6f2f4",
+                          border: "#e6f2f4",
+                        }}
+                      >
                         <CardBody>
                           <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
                             <i className="ni ni-like-2" />
                           </div>
                           <h6 className="text-primary text-uppercase">
-                            Health 
+                            Health
                           </h6>
                           <p className="text-primary text-uppercase">
                             Shop Now
@@ -116,7 +133,13 @@ class Billboard extends React.Component {
                       </Card>
                     </Col>
                     <Col lg="2" className="featured-category">
-                      <Card className="col mb-5" style={{backgroundColor: "#fff6f6", border: "#fff6f6"}}>
+                      <Card
+                        className="col mb-5"
+                        style={{
+                          backgroundColor: "#fff6f6",
+                          border: "#fff6f6",
+                        }}
+                      >
                         <CardBody>
                           <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
                             <i className="ni ni-hat-3" />
@@ -135,21 +158,39 @@ class Billboard extends React.Component {
               </Row>
             </Container>
           </section>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
+          <section className="section section-components pb-0 pt-0">
+            <Container className="col-md-11 col-lg-11">
+              <Row className="row-grid justify-content-between align-items-end">
+                <h2 className="mt-md mb-5 display-4">
+                  <span>Bestselling Books</span>
+                </h2>
+                <h6 className="mt-lg mb-5">
+                  <span>View All</span> <i className="fa fa-chevron-right"></i>
+                </h6>
+              </Row>
+              <Row className="justify-content-center">
+                <Col lg="12">
+                  <Row className="row-grid">
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
         </main>
       </>
     );
